6a9089127aabc1a2fe6c68cb768f98ebc800c86c
Mirza Ahtsham <mirza.ahtsham786@outlook.com>
2024-05-20 18:02:27 +0500

    Add Some Code Header Designing

--- src/components/header.jsx
+++ src/components/header.jsx
@@ -1,5 +1,9 @@
 // src/components/Header.jsx
-import Logo from '../assets/medmax-logo.png';
+import { IoMdSearch } from "react-icons/io";
+import { MdOutlineMailOutline } from "react-icons/md";
+import { IoIosArrowForward } from "react-icons/io";
+import { FaPhoneAlt } from "react-icons/fa";
+import logo from '../assets/medmax-logo.png';
 import React from 'react';
 import { IoMenuOutline } from "react-icons/io5";
 
@@ -13,18 +17,44 @@ const Header = () => {
                 </div>
             </div>
             <div className="middle">
-                <div className="headerlogo flex">
-                    <div className="mobile-menu-opener lg:hidden text-4xl text-[#284469] px-11 py-11"><IoMenuOutline />
-                    <div className="logo"><img className="w-14" src="../assets/medmax-logo.png" alt="" /></div>
+                <div className="headerlogo flex items-center justify-between px-5  ">
+                    <div className="mobile-menu-opener lg:hidden text-4xl text-[#284469] "><IoMenuOutline /></div>
+                    <div className="logo lg:hidden"><img className="h-5" src={logo} alt="" /></div>
+                    <div className="left hidden lg:block">
+                        <div className="px-5 py-8 hidden lg:block"><img className="h-8" src={logo} alt="" /></div>
                     </div>
-                    <div className="left">
-                        <img src="../assets/medmax-logo.png" alt="" />
+                    <div className="right hidden lg:flex space-x-4 ">
+                        <div className="contact-info items-center">
+                            <div className="flex flex-col"><FaPhoneAlt className="text-2xl" />
+                                <p className="text-sm font-bold ">Contact Us</p>
+                                <p className="text-xs">+123 456 789 02</p>
+                            </div>
+                        </div>
+                        <div className="contact-info items-center">
+                            <MdOutlineMailOutline className="text-2xl flex flex-col" />
+                            <p className="text-sm font-bold">Email</p>
+                            <p className="text-xs">email@email.com</p>
+                        </div>
+                        <div className="contact-info items-center">
+                            <button><IoIosArrowForward className="text-2xl flex flex-col" />
+                                <p className="text-sm font-bold">Contact Us</p></button>
+                        </div>
                     </div>
-                    <div className="right hidden lg:relative lg:flex space-x-10 items-baseline justify-start ml-4">
-                        <div className="email">email</div>
-                        <div className="contact">conatct</div>
-                        <div className="button">button</div>
-                    </div>
+                </div>
+            </div>
+            <div className="nav px-2 py-2">
+                <div className="right flex-col hidden lg:block">
+                    <ul>
+                        <li>Home</li>
+                        <li>Pages</li>
+                        <li>Doctors</li>
+                        <li>Blog</li>
+                        <li>Contact</li>
+                    </ul>
+                </div>
+                <div className="left">
+                <div className="mobile-menu-opener hidden lg:block text-4xl text-[#284469] "><IoMdSearch /></div>
+                <div className="mobile-menu-opener hidden lg:block text-4xl text-[#284469] "><IoMenuOutline /></div>
                 </div>
             </div>
         </header>
